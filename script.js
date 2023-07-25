42f1c940cb6bd168a983eb99d2ad792b3341fdb5
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-25 12:14:49 -0400

    objects are being created, next is to return them on click

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
 const createPeople = (function(){
     const startBtn = document.querySelector('button');
     const player1Name = document.querySelector('#p1-name');
-    // const player1X = document.querySelector('#p1-x');
-    // const player1O = document.querySelector('#p1-o');
     const player2Name = document.querySelector('#p2-name');
-    // const player2X = document.querySelector('#p2-x');
-    // const player2O = document.querySelector('#p2-o');
     const assignments = document.querySelectorAll('input[type="radio"]');
 
     const makePlayer = (name,assignment) => {
         return {name,assignment};
     };
 
+    let assignmentsArray = [];
+
     const getAssignment = () => {
-        //return only the index value that return true
-        let assignmentsArray = [];
+        assignmentsArray = [];
         assignments.forEach( index => {
             if(index.checked){
                 assignmentsArray.push(index.dataset.player)
             }
         })
-        const player1Assignment = assignmentsArray[0];
-        const player2Assignment = assignmentsArray[1];
-        return {player1Assignment, player2Assignment}
     }
-
-    console.log(getAssignment.player1Assignment)
     
-    // let player1Assignment = giveSelection();
-    // let player2Assignment = giveSelection();
+    startBtn.addEventListener('click', () => {  
+        getAssignment()
+        let player1 = makePlayer(player1Name.value, assignmentsArray[0]);
+        let player2 = makePlayer(player2Name.value, assignmentsArray[1]);
+        console.log(player1, player2)
+        })
     
-    let player1 = makePlayer(player1Name.value);
-    let player2 = makePlayer(player2Name.value);
-    
-    console.log(player1,player2)
-    
-    
+    // console.log(player1,player2)
+    // return {player1, player2}
 })()
 
 
-//the main goal of this code is to return 2 objects that are different.
 
-// assignments.forEach(index => {
-//     if(index.checked){
-//         const playerAssignment = index.dataset.player;
-//         console.log(playerAssignment)
-//     }
-// })
