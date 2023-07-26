be2692a87c8eaffaddbb10aed565680ae13069fe
Jdoggett50 <dillondoggett5@gmail.com>
2023-07-26 07:17:06 -0400

    reworking the player creation process

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-const createPeople = (function(){
-    const startBtn = document.querySelector('button');
-    const player1Name = document.querySelector('#p1-name');
-    const player2Name = document.querySelector('#p2-name');
-    const assignments = document.querySelectorAll('input[type="radio"]');
+//function expressions are not available throughout your code
+//function expressions are used in effort to avoid polluting the 
+//global namespace. That being said, I think that is what this lesson
+//is trying to teach. How to write functions without polluting the
+//global namespace and how to utilize factory functions for memory
+//management. It wants me to utilize modules for the game
+//functionality and it wants me to utilize factory functions to create
+//players
 
-    const makePlayer = (name,assignment) => {
-        return {name,assignment};
-    };
-
-    let assignmentsArray = [];
-
-    const getAssignment = () => {
-        assignmentsArray = [];
-        assignments.forEach( index => {
-            if(index.checked){
-                assignmentsArray.push(index.dataset.player)
-            }
-        })
+//because the data is returned from the function, it's data is
+//accessible globally.
+const getPlayer = (name, assignment) => {
+    return {
+        name,
+        assignment,
     }
-    
-    startBtn.addEventListener('click', () => {  
-        getAssignment()
-        let player1 = makePlayer(player1Name.value, assignmentsArray[0]);
-        let player2 = makePlayer(player2Name.value, assignmentsArray[1]);
-        console.log(player1, player2)
-        })
-    
-    // console.log(player1,player2)
-    // return {player1, player2}
-})()
+}   
 
+//remember that returning the data from the function, allow access
+//to that data.
+const setPlayer = () => {
+    const p1Name = document.querySelector('#p1-name');
+    let player1 = getPlayer('john', 'x');
+    console.log(player1)
+}
 
-
+setPlayer()
