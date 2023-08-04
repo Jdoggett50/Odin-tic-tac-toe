e809de8c6052b9072f8934e24dbce182188a651a
Jdoggett50 <dillondoggett5@gmail.com>
2023-08-04 14:30:55 -0400

    receiving the assignment and inserting the assignment both work

--- script.js
+++ script.js
@@ -20,34 +20,59 @@ const boardModule = (()=>{
 const player1 = {name:'john', assignment:'x',};
 const player2 = {name:'haleigh', assignment:'o',};
 const players = [player1,player2];
-const newGame = boardModule.getBoard();
-newGame[4].push(player1.assignment)
-newGame[2].push(player2.assignment)
 
 const getAssignment = (arr,players) => {
     const count = arr.filter(index => index != '')
     console.log(count.length)
-    console.log(Boolean((count.length % 2) + 1 === 0))
-    if(count.length === 0 || count.length % 2 + 1 === 0){
+    if(count.length === 0 || count.length % 2 === 0){
         return players[0].assignment;
     } else 
         return players[1].assignment;
 }
 
-console.log(getAssignment(boardModule.getBoard(),players))
+//when the player selects an array space, 
+//getAssignment(boardModule.getBoard(),players)
+// ^^ gives the gameController the correct player based on the boards
+// length
 
 const gameController = (() => {
     //this monitors the state of the board and insertions into the board
+    // const newGame = boardModule.getBoard();
 
-    const setPlayer = (name, assignment) => { 
+    const winner = false;
+
+    const _setPlayer = (name, assignment) => { 
         return {
-            name, assignment
+            name, 
+            assignment
         }
     }
+
+    const _getWin = (arr) => {
+        //if x or o is represented in only these spots, there is a 
+        //winner
+        //
+    }
+
+    const _checkWin = (arr) => {
+        console.log(arr)
+        //this will check if the game is won, if the game is won
+        //display a message saying whichever player is the winner
+        //compare the two player's assignments for the winner's value
+    }
     
+    const playRound = () => {
+
+    }
+
+    const startGame = (arr) => {
+        _checkWin(newGame)
+        //this will cycle through the turns
+        //place the inputs inside the game
+    }
 
     return {
-        setPlayer,
+        
     }
 })()
 
