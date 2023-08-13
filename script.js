fda85eb93cec48fb3ce4bba976191640616b4eaf
Jdoggett50 <dillondoggett5@gmail.com>
2023-08-13 06:51:26 -0400

    submit button is working with the player assignments and square event is firing correct assignments

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const boardModule = (()=>{
     const board = [[],[],[],[],[],[],[],[],[]];
     
-    //returns created board for use in another function
     const getBoard = () => board;
     
-    //takes the selection index and assignment and places it inside arr
     const insertAssignment = (selection,assignment) => {
         if(board[selection].length < 1){
             return board[selection].push(assignment);
@@ -18,65 +16,8 @@ const boardModule = (()=>{
 })()
 
 const gameController = (() => {
-    //switches player values
-    const _cyclePlayers = (arr,players) => {
-        const count = arr.filter(index => index != '')
-        if(count.length === 0 || count.length % 2 === 0){
-            return players[0].assignment;
-        } else 
-        return players[1].assignment;
-    }
-    
-    //returns a boolean whether a win condition is met
-    const _checkWin = (arr) => {
-        let gameWinner = false
-        const winCases = [
-            [0,1,2],[3,4,5],[6,7,8],
-            [0,3,6],[1,4,7],[2,5,8],
-            [0,4,8],[2,4,6]
-        ];
-        
-        for(const [a,b,c] of winCases){
-            if(arr[a].toString() === '' && arr[b].toString() === '' && arr[c].toString() === arr[b].toString()){
-                return gameWinner
-            }   else if (arr[a].toString() === arr[b].toString() && arr[b].toString() === arr[c].toString()) {
-                gameWinner = arr[a].toString(); 
-            }
-        }
-        return gameWinner
-    };
 
-    //looks at tie on each iteration of the game
-    const _checkTie = (arr) => {
-        if(arr.filter(element => element !== '') && !_checkWin(arr)){
-            return true
-        }
-    }
-
-    //will receive t/f value to announce winners, value will be give to screencontroller if true
-    const _getWinner = (assignment,players) => {
-        for(const player of players){
-            if (assignment === player.assignment){
-                return player.name;
-            }
-        };
-    };
-
-    
-    const playRound = (selection,arr) => {
-        boardModule.insertAssignment(selection, _cyclePlayers(arr,players))
-        _checkWin(arr)
-        _checkTie(arr)
-    }
-
-    return {
-        playRound,
-    }
-})()
-
-const screenController = (() => {
-    //template for creating players
-    const _makePlayers = () => {
+    const getPlayers = () => {
         const _getInfo = (name, assignment) => {
             return {
                 name,
@@ -94,7 +35,6 @@ const screenController = (() => {
             return p2Name.value;
         }
 
-        //receives assignments from DOM
         const _getAssignments = () => {
             const radioAssignments = document.querySelectorAll('input[type="radio"]');
             let assignmentsArray = [];
@@ -110,7 +50,6 @@ const screenController = (() => {
         const p1Assignment = assignments[0];
         const p2Assignment = assignments[1];
 
-        //checks for assignment equality
         const checkAssignments = () => {
             let assignBool = true;
             if (p1Assignment == p2Assignment){
@@ -120,10 +59,9 @@ const screenController = (() => {
             return assignBool;
         }
 
-        //sets the players if the assignments aren't the same
         const setPlayers = () => {
             if(checkAssignments()){
-                _hideForm();
+                screenController.hideForm();
                 const player1 = _getInfo(_getP1Name(), p1Assignment);
                 const player2 = _getInfo(_getP2Name(), p2Assignment);
                 return {
@@ -135,49 +73,90 @@ const screenController = (() => {
         return setPlayers();
     }
 
-    //this will double in functionality. if form is active I want to hide game. 
-    //if game is active I want to hide form
-    const _hideForm = () => {
+    //in this case, arr is the gameBoard arr
+    const _cyclePlayers = (arr) => {
+        const count = arr.filter(index => index != '')
+        if(count.length === 0 || count.length % 2 === 0){
+            return gameController.getPlayers().player1.assignment;
+        } else 
+        return gameController.getPlayers().player2.assignment;
+    }
+    
+    const _checkWin = (arr) => {
+        let gameWinner = false;
+        const winCases = [
+            [0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8],[2,4,6]
+        ];
+        
+        for(const [a,b,c] of winCases){
+            if(arr[a].toString() === '' && arr[b].toString() === '' && arr[c].toString() === arr[b].toString()){
+                return gameWinner
+            }   else if (arr[a].toString() === arr[b].toString() && arr[b].toString() === arr[c].toString()) {
+                gameWinner = arr[a].toString(); 
+            }
+        }
+        return gameWinner
+    };
+
+    const _checkTie = (arr) => {
+        if(arr.filter(element => element !== '') && !_checkWin(arr)){
+            return true
+        }
+    };
+
+    const _getWinner = (assignment,players) => {
+        for(const player of players){
+            if (assignment === player.assignment){
+                return player.name;
+            }
+        };
+    };
+
+    
+    const playRound = (selection,arr) => {
+        boardModule.insertAssignment(selection, _cyclePlayers(arr))
+            _checkWin(arr);
+            console.log(_checkWin(boardModule.getBoard()))
+            _checkTie(arr);
+            console.log(_checkTie(boardModule.getBoard()))
+        }
+
+        const squareClick = () => {
+            const boardWrapper = document.querySelector('.board-wrapper');
+            boardWrapper.addEventListener('click', (evt) => {
+            boardModule.insertAssignment(evt.target.dataset.index,_cyclePlayers(boardModule.getBoard()))
+                console.log(boardModule.getBoard())
+            });
+        }
+        squareClick()
+
+    return {
+        getPlayers,
+    }
+})()
+
+const screenController = (() => {
+    //on each turn, listen for the winner
+    //look over the array and update the board with the values in 
+    //the given array
+
+    const hideForm = () => {
         const form = document.querySelector('.form-wrapper');
         form.classList.add('no-show');
     }
 
     const submitClick = () => {
-        //create the player
         const submitBtn = document.querySelector('button');
-        submitBtn.addEventListener('click', ()=> {
-            _makePlayers();
+        submitBtn.addEventListener('click', () => {
+            gameController.getPlayers();
         })
     }
 
-    const squareClick = () => {
-        const boardWrapper = document.querySelector('.board-wrapper');
-        boardWrapper.addEventListener('click', (evt)=> {
-            
-            playRound(evt.target.dataset.index,)
-        });
-    }
-
-    //remove this
-    
     submitClick()
-    squareClick()
-
+    
+    return {
+        hideForm,
+    }
 })() 
-
-/* set the data in one function for use in another function.
-on submit click, store the data.
-
-pseudocode is as follows:
-function playGame () {
-		//data that derives from makePlayers()
-		const newBoard = instance of the boardModule;
-		const newPlayers = instance of this games players as an array
-		const playRound() = () => {
-				// this will execute the functions with the data inside 
-		}
-		return playRound
-}
-
-in short playGame will create the instance of game and playround will be returned as a 
-method of playGame where it will use the stored data from the submission to play a single round.*/
