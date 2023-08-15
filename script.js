a9ebb53739aa776e269032ee45215e76ee81f437
Jdoggett50 <dillondoggett5@gmail.com>
2023-08-15 12:56:22 -0400

    added truthy check to first existing arr element in checkWin fn

--- script.js
+++ script.js
@@ -85,12 +85,15 @@ const gameController = (() => {
         const winCases = [ 
             [0,1,2],[3,4,5],[6,7,8],
             [0,3,6],[1,4,7],[2,5,8],
-            [0,4,8],[2,4,6],
+            [0,4,8],[2,4,6]
         ]
-        winCases.forEach(win => {
+        for(const win of winCases){
             const [a,b,c] = win;
-            console.log(win,a,b,c)
-        });
+            if(arr[a] && arr[b] === arr[a] && arr[b] === arr[c]){
+                return arr[a];
+            }
+        }
+        return false;
     }
 
     const checkTie = (arr) => {
